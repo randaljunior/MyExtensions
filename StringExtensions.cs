a1e669e6039237b546125d083835e01206d36549
Randal Junior <randal.junior@gmail.com>
2025-04-11 16:21:50 -0300

    Commit 20250411

--- StringExtensions.cs
+++ StringExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text;
 using System.Text.RegularExpressions;
 
@@ -8,12 +9,12 @@ namespace MyExtensions
     {
         public static string Left(this string value, int length)
         {
-            return value.Substring(0, Math.Min(length, value.Length));
+            return value?.Substring(0, Math.Min(length, value.Length));
         }
 
         public static string Right(this string value, int length)
         {
-            return value.Substring(Math.Max(value.Length - 1, value.Length - length - 1));
+            return value?.Substring(Math.Max(value.Length - 1, value.Length - length - 1));
         }
 
         public static string RegexReplace(this string value, string regexOldString, string newString)
@@ -22,11 +23,19 @@ namespace MyExtensions
             return rgx.Replace(value, newString);
         }
 
-        public static string UTF8toHex(this string value)
+        public static string Utf8ToHex(this string value)
         {
             byte[] vs = Encoding.UTF8.GetBytes(value);
             var hexValue = BitConverter.ToString(vs).Replace("-", "");
             return hexValue;
         }
+
+        public static string HexToUtf8(this string value)
+        {
+            byte[] vs = Encoding.UTF8.GetBytes(value);
+            var hexValue = BitConverter.ToString(vs).Replace("-", "");
+            return hexValue;
+        }
+
     }
 }
