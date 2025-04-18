3b881b8d5f6d6f6e940125dfd68a9bc6eef82dce
Randal Junior <randal.junior@gmail.com>
2025-04-18 19:13:09 -0300

    Otimizações e melhorias

--- StringExtensions.cs
+++ StringExtensions.cs
@@ -1,80 +1,180 @@
 ﻿using System;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Text.Unicode;
 
 namespace MyExtensions
 {
     public static class StringExtensions
     {
+
+        /// <summary>
+        /// Checks if a string is null or empty.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
         public static bool IsNullOrEmpty(this string? value)
         {
             return String.IsNullOrEmpty(value);
         }
 
+        /// <summary>
+        /// Checks if a string is null or empty or whitespace.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
         public static bool IsNullOrWhiteSpace(this string? value)
         {
             return String.IsNullOrWhiteSpace(value);
         }
 
-        public static string Repeat(this string value, int size)
+        /// <summary>
+        /// Repeat a string n times.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="ntimes"></param>
+        /// <returns></returns>
+        public static string Repeat(this string value, int ntimes)
         {
-            Span<char> _text = stackalloc char[value.Length * size];
-            for (int i = 0; i < size; i++)
-            {
-                value.AsSpan().CopyTo(_text.Slice(i * value.Length, value.Length));
-            }
+            if (string.IsNullOrEmpty(value) || ntimes <= 0)
+                return string.Empty;
 
-            return _text.ToString();
+            //Span<char> _text = stackalloc char[value.Length * ntimes];
+            //for (int i = 0; i < ntimes; i++)
+            //{
+            //    value.AsSpan().CopyTo(_text.Slice(i * value.Length, value.Length));
+            //}
+
+            //return _text.ToString();
+
+            return string.Create(value.Length * ntimes, (value, ntimes), (span, state) =>
+            {
+                var (_value, _repetitions) = state;
+                for (int i = 0; i < _repetitions; i++)
+                {
+                    _value.AsSpan().CopyTo(span.Slice(i * _value.Length));
+                }
+            });
         }
 
-        public static string Repeat(this char value, int size)
-        {
-            Span<char> _text = stackalloc char[size];
-            for (int i = 0; i < size; i++)
-            {
-                _text[i] = value;
-            }
 
-            return _text.ToString();
+        /// <summary>
+        /// Repeat a char n times.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="ntimes"></param>
+        /// <returns></returns>
+        public static string Repeat(this char value, int ntimes)
+        {
+            if (ntimes <= 0 || value == default)
+                return string.Empty;
+
+            //Span<char> _text = stackalloc char[size];
+            //for (int i = 0; i < size; i++)
+            //{
+            //    _text[i] = value;
+            //}
+
+            //return _text.ToString();
+
+            return string.Create(ntimes, (value, ntimes), (span, state) =>
+            {
+                var (_value, _repetitions) = state;
+                for (int i = 0; i < _repetitions; i++)
+                {
+                    span[i] = _value;
+                }
+            });
+
         }
 
+        /// <summary>
+        /// Returns the left part of a string.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="lenght"></param>
+        /// <returns></returns>
         public static ReadOnlySpan<char> Left(this ReadOnlySpan<char> input, int lenght)
         {
             return input.Slice(0, Math.Min(lenght, input.Length));
         }
 
+        /// <summary>
+        /// Returns the left part of a string. 
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="lenght"></param>
+        /// <returns></returns>
         public static string Left(this string input, int lenght)
         {
             return input.AsSpan().Slice(0, Math.Min(lenght, input.Length)).ToString();
         }
 
+        /// <summary>
+        /// Returns the right part of a string.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="lenght"></param>
+        /// <returns></returns>
         public static ReadOnlySpan<char> Right(this ReadOnlySpan<char> input, int lenght)
         {
             return input.Slice(Math.Max(input.Length - 1, input.Length - lenght - 1));
         }
 
+        /// <summary>
+        /// Returns the right part of a string.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="lenght"></param>
+        /// <returns></returns>
         public static string? Right(this string input, int lenght)
         {
             return input.AsSpan().Slice(Math.Max(input.Length - 1, input.Length - lenght - 1)).ToString();
         }
 
+        /// <summary>
+        /// Replace a string using regex.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="regexOldString"></param>
+        /// <param name="newString"></param>
+        /// <returns></returns>
         public static string RegexReplace(this string value, string regexOldString, string newString)
         {
             var rgx = new Regex(regexOldString);
             return rgx.Replace(value, newString);
         }
 
+        /// <summary>
+        /// Converts a string to hexadecimal using UTF-8 encoding.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
         public static string Utf8ToHex(this string value)
         {
             return Utf8ToHex(value.AsSpan());
         }
 
+        /// <summary>
+        /// Converts a string to hexadecimal using UTF-8 encoding.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
         public static string Utf8ToHex(this ReadOnlySpan<char> input)
         {
-            Span<byte> utf8Bytes = stackalloc byte[Encoding.UTF8.GetByteCount(input)];
+            if (input.IsEmpty)
+                return string.Empty;
+
+            var _size = Encoding.UTF8.GetByteCount(input);
+
+            Span<byte> utf8Bytes =
+                (_size < 1024)
+                ? stackalloc byte[_size]
+                : new byte[_size];
+
             _ = Encoding.UTF8.GetBytes(input, utf8Bytes);
 
-            return Convert.ToHexString(utf8Bytes).Replace("-", "");
+            return Convert.ToHexString(utf8Bytes);
         }
 
         // TODO: Melhorar a conversão de hexadecimal para UTF-8
