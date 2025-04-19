b9816f08996c3a5a9e56189ea4959744750133ac
Randal Junior <randal.junior@gmail.com>
2025-04-18 21:10:01 -0300

    Otimização e Melhorias.

--- HashExtensions.cs
+++ HashExtensions.cs
@@ -9,26 +9,16 @@ namespace MyExtensions
     {
         public static string ComputeHashSHA512(this string input)
         {
-            using (SHA512 SHA512 = new SHA512Managed())
-            {
-                var hash = SHA512.ComputeHash(Encoding.UTF8.GetBytes(input));
-
-                var str = Helpers.ByteArrayToHexViaLookup32(hash);
-
-                return str;
-            }
+            ArgumentNullException.ThrowIfNull(input);
+            Span<byte> hash = SHA512.HashData(Encoding.UTF8.GetBytes(input));
+            return Helpers.ByteArrayToHexViaLookup32(hash);
         }
 
         public static string ComputeHashSHA1(this string input)
         {
-            using (SHA1 SHA1 = new SHA1Managed())
-            {
-                var hash = SHA1.ComputeHash(Encoding.UTF8.GetBytes(input));
-
-                var str = Helpers.ByteArrayToHexViaLookup32(hash);
-
-                return str;
-            }
+            ArgumentNullException.ThrowIfNull(input);
+            Span<byte> hash = SHA1.HashData(Encoding.UTF8.GetBytes(input));
+            return Helpers.ByteArrayToHexViaLookup32(hash);
         }
 
 
@@ -36,30 +26,34 @@ namespace MyExtensions
 
     public static class Helpers
     {
-        internal static readonly uint[] _lookup32 = CreateLookup32();
+        private static readonly uint[] Lookup32 = CreateLookup32();
 
         private static uint[] CreateLookup32()
         {
-            var result = new uint[256];
+            var lookup = new uint[256];
             for (int i = 0; i < 256; i++)
             {
                 string s = i.ToString("X2");
-                result[i] = ((uint)s[0]) + ((uint)s[1] << 16);
+                lookup[i] = ((uint)s[0]) + ((uint)s[1] << 16);
             }
-            return result;
+            return lookup;
         }
 
-        internal static string ByteArrayToHexViaLookup32(byte[] bytes)
+        internal static string ByteArrayToHexViaLookup32(Span<byte> bytes)
         {
-            var lookup32 = _lookup32;
-            var result = new char[bytes.Length * 2];
+            Span<char> result =
+                (bytes.Length * 2) <= 1024
+                    ? stackalloc char[bytes.Length * 2]
+                    : new char[bytes.Length * 2];
+
             for (int i = 0; i < bytes.Length; i++)
             {
-                var val = lookup32[bytes[i]];
+                var val = Lookup32[bytes[i]];
                 result[2 * i] = (char)val;
                 result[2 * i + 1] = (char)(val >> 16);
             }
-            return new string(result);
+
+            return result.ToString();
         }
     }
 }
