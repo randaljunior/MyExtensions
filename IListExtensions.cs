138b1da9c4f3f251224cc0028381908793d32a25
Randal Junior <randal.junior@gmail.com>
2025-04-18 21:19:08 -0300

    Otimização e Melhorias

--- IListExtensions.cs
+++ IListExtensions.cs
@@ -6,13 +6,31 @@ namespace MyExtensions
 {
     public static partial class IListExtensions
     {
-        public static void ReplaceItem<Tsource>(this IList<Tsource> list, Tsource newItem, Func<Tsource, bool> Selector)
+        public static void ReplaceItem<TSource>(this IList<TSource> list, TSource newItem, Func<TSource, bool> selector)
         {
-            var _oldItem = list.First(Selector);
+            ArgumentNullException.ThrowIfNull(list);
+            ArgumentNullException.ThrowIfNull(selector);
 
-            if (_oldItem != null)
+            if (list is List<TSource> lst)
             {
-                list[list.IndexOf(_oldItem)] = newItem;
+                int index = lst.FindIndex(new Predicate<TSource>(selector));
+                if (index != -1)
+                {
+                    lst[index] = newItem;
+                    return;
+                }
+            }
+            else
+            {
+                int count = list.Count;
+                for (int i = 0; i < count; i++)
+                {
+                    if (selector(list[i]))
+                    {
+                        list[i] = newItem;
+                        return;
+                    }
+                }
             }
         }
     }
